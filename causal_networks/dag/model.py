37338b0f11db0f5b52e181312a08634fa5251050
Sam Adam-Day <me@samadamday.com>
2023-07-06 12:07:50 +0100

    Finished vectorised dag code

--- causal_networks/dag/model.py
+++ causal_networks/dag/model.py
@@ -1,4 +1,5 @@
 from typing import Any, Optional
+from functools import partial
 
 import torch
 
@@ -22,13 +23,18 @@ class DAGModel(HookedRootModule):
     def __init__(self):
         super().__init__()
         self.graph = nx.DiGraph()
+        self.visualization_layout = None
 
     def add_node(
         self,
         name: str,
         module: DAGModule,
+        **node_attributes: Any,
     ):
         """Add a node to the DAG
+
+        Runs the `setup` method each time after adding the node, to update the list of
+        hooks.
 
         Parameters
         ----------
@@ -37,9 +43,16 @@ class DAGModel(HookedRootModule):
         module : DAGModule
             The module to be used as the node, which computes its value based on the
             values of its parents
+        **node_attributes : Any
+            Any additional attributes to be added to the node
         """
-        self.graph.add_node(name, module=module)
+        hook_point = HookPoint()
+        self.graph.add_node(
+            name, module=module, hook_point=hook_point, **node_attributes
+        )
         self.add_module(name, module)
+        self.add_module(f"hook_{name}", hook_point)
+        self.setup()
 
     def add_edge(self, node1: str, node2: str):
         """Add an edge to the DAG
@@ -87,8 +100,25 @@ class DAGModel(HookedRootModule):
         """
         return [node for node in self.graph.nodes if self.graph.out_degree(node) == 0]
 
-    def forward(self, inputs: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
-        """Forward pass through the DAG"""
+    def forward(
+        self, inputs: dict[str, torch.Tensor], output_type="output_nodes"
+    ) -> dict[str, torch.Tensor]:
+        """Forward pass through the DAG
+
+        Parameters
+        ----------
+        inputs : dict[str, torch.Tensor]
+            The input values for the input nodes. The values can be batched.
+        output_type : str, default="output_nodes"
+            The type of output to return. Can be one of:
+                - "output_nodes": only the output nodes
+                - "all_nodes": all node values
+
+        Returns
+        -------
+        outputs : dict[str, torch.Tensor]
+            The values for the output nodes. Batched in the same way as the inputs.
+        """
 
         # Compute the values of the nodes
         for node in nx.topological_sort(self.graph):
@@ -105,14 +135,139 @@ class DAGModel(HookedRootModule):
                     parent: self.graph.nodes[parent]["module"].value
                     for parent in parents
                 }
-                self.graph.nodes[node]["module"].forward(parent_values)
+                self.graph.nodes[node]["module"].forward(
+                    parent_values, hook_point=self.graph.nodes[node]["hook_point"]
+                )
 
-        # Return the values of the output nodes
-        ouput_nodes = self.get_output_nodes()
-        return {node: self.graph.nodes[node]["module"].value for node in ouput_nodes}
+        # Return the values of the nodes
+        if output_type == "output_nodes":
+            output_nodes = self.get_output_nodes()
+            return {
+                node: self.graph.nodes[node]["module"].value for node in output_nodes
+            }
+        elif output_type == "all_nodes":
+            return {node: self.graph.nodes[node]["module"].value for node in self.graph}
+        else:
+            raise ValueError(f"Unknown output type {output_type}")
+
+    def run_interchange_intervention(
+        self,
+        base_input: dict[str, torch.Tensor],
+        source_inputs: dict[str, torch.Tensor],
+        intervention_mask: dict[str, torch.BoolTensor],
+        output_type="output_nodes",
+    ) -> dict[str, torch.Tensor]:
+        """Run interchange intervention with base and source inputs
+
+        Runs the forward pass with the source inputs, recording the values of the nodes.
+        Then runs the forward pass with the base inputs, replacing the values of the
+        nodes with the recorded values according to the intervention mask.
+
+        All inputs must be batched. The source inputs and intervention mask must have a
+        second batch dimension for the number of source inputs.
+
+        See below for how the intervention mask is broadcasted to match the values. This
+        can allow for more fine-grained intervention than just per node.
+
+        Parameters
+        ----------
+        base_input : dict[str, torch.Tensor of shape (batch_size, ...)]
+            The base input values for the input nodes
+        source_inputs : dict[str, torch.Tensor of shape (batch_size, num_source_inputs,
+        ...)]
+            The source input values for the input nodes
+        intervention_mask : dict[str, torch.BoolTensor of shape (batch_size,
+        num_source_inputs, ...)]
+            The intervention mask for the nodes. Any nodes not appearing as keys will
+            not be intervened on. For each node and source input `i`, the mask value at
+            `[:, i, ...]` will have dimensions added to match the value, and will then
+            be broadcasted to match the value (i.e. we do left-to-right broadcasting).
+            This means that a 2D mask will allow intervening per node, while
+            higher-dimensional masks will allow more fine-grained intervention.
+        output_type : str, default="output_nodes"
+            The type of output to return. Can be one of:
+                - "output_nodes": only the output nodes
+                - "all_nodes": all node values
+
+        Returns
+        -------
+        outputs : dict[str, torch.Tensor of shape (batch_size, ...)]
+            The values for the nodes. Batched in the same way as the inputs.
+        """
+
+        source_values = self.forward(source_inputs, output_type="all_nodes")
+
+        # An intervention hook which replaces the value with the source value according
+        # to the intervention mask
+        def intervention_hook(
+            value: torch.Tensor,
+            hook: HookPoint,
+            node: str,
+            source_values: dict[str, torch.Tensor],
+            intervention_mask: dict[str, torch.Tensor],
+        ):
+            # If the node is not in the intervention mask, don't intervene
+            if node not in intervention_mask:
+                return value
+
+            # Add trailing dimensions to the intervention mask to match the value plus
+            # the source input dimension
+            node_intervention_mask = intervention_mask[node]
+            node_intervention_mask = node_intervention_mask.view(
+                *node_intervention_mask.shape,
+                *[1] * (1 + value.ndim - node_intervention_mask.ndim),
+            )
+
+            # For each source input, replace the value with the source value according
+            # to the broadcasted intervention mask
+            for i in range(source_values[node].shape[1]):
+                value = torch.where(
+                    intervention_mask[node][:, i, ...],
+                    source_values[node][:, i, ...],
+                    value,
+                )
+
+            return value
+
+        # Register the intervention hooks
+        fwd_hooks = []
+        for node in self.graph:
+            fwd_hooks.append(
+                (
+                    f"hook_{node}",
+                    partial(
+                        intervention_hook,
+                        node=node,
+                        source_values=source_values,
+                        intervention_mask=intervention_mask,
+                    ),
+                )
+            )
+
+        # Run the forward pass with the base input and the intervention hooks
+        output = self.run_with_hooks(
+            base_input, fwd_hooks=fwd_hooks, output_type=output_type
+        )
+
+        return output
+    
+    def set_visualization_layout(self, layout_function: Optional[callable]):
+        """Set the layout function for visualizing the DAG
+
+        Parameters
+        ----------
+        layout_function : callable
+            The layout function to use for drawing the graph. It is recommended to use
+            a function from `networkx.drawing.layout` (possibly wrapped in a partial).
+        """
+        self.visualization_layout = layout_function
 
     def visualize(
-        self, display_node_info=True, display_node_values=False, cmap_name="Pastel1"
+        self,
+        display_node_info=True,
+        display_node_values=False,
+        layout: Optional[callable] = None,
+        cmap_name="Pastel1",
     ):
         """Draw a visualization of the DAG
 
@@ -122,13 +277,28 @@ class DAGModel(HookedRootModule):
             Whether to display information about the nodes in a table
         display_node_values : bool, default=False
             Whether to display the values of the nodes in the table
+        layout : callable, optional
+            The layout function to use for drawing the graph. If None, uses the
+            `visulaization_layout` attribute if available, otherwise uses the
+            Kamada-Kawai path-length cost-function
         cmap_name : str, default="Pastel1"
             The name of the colormap to use
         """
 
         cmap = colormaps[cmap_name]
 
-        nx.draw_kamada_kawai(self.graph, with_labels=True, node_color=cmap(1))
+        if layout is None:
+            if self.visualization_layout is not None:
+                layout = self.visualization_layout
+            else:
+                layout = nx.kamada_kawai_layout
+
+        nx.draw(
+            self.graph,
+            pos=layout(self.graph),
+            with_labels=True,
+            node_color=[cmap(1)] * len(self.graph.nodes),
+        )
         plt.show()
 
         if display_node_info:
--- causal_networks/dag/module.py
+++ causal_networks/dag/module.py
@@ -1,27 +1,29 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
 import torch
 from torch import nn
 
+from transformer_lens.hook_points import HookPoint
+
 
 class DAGModule(nn.Module, ABC):
     """A module that can be used as a node in a DAG model"""
-    
-    def __init__(self):
+
+    def __init__(self, **kwargs):
         super().__init__()
+        self._init_kwargs = kwargs
         self.value = None
 
     @abstractmethod
-    def forward(self, parent_values: dict[str, torch.Tensor]) -> torch.Tensor:
+    def compute_value(self, parent_values: dict[str, torch.Tensor]) -> torch.Tensor:
         """Compute the value of the node based on the values of its parents
 
-        Records the value of the node in the `value` attribute, and returns it.
-        
         Parameters
         ----------
         parent_values : dict[str, torch.Tensor]
             The values of the parents of the node
-        
+
         Returns
         -------
         value : torch.Tensor
@@ -29,44 +31,99 @@ class DAGModule(nn.Module, ABC):
         """
         pass
 
+    def forward(
+        self, parent_values: dict[str, torch.Tensor], hook_point: Optional[HookPoint]
+    ) -> torch.Tensor:
+        """Do a forward pass through the module
+
+        Uses the `compute_value` method to compute the value of the node based on the
+        values of its parents. Optionally passes this through a hook point. Records this
+        value in the `value` attribute, and returns it.
+
+        Parameters
+        ----------
+        parent_values : dict[str, torch.Tensor]
+            The values of the parents of the node
+        hook_point : HookPoint, optional
+            A hook point to pass the value through, by default None
+
+        Returns
+        -------
+        value : torch.Tensor
+            The value of the node
+        """
+        self.value = self.compute_value(parent_values)
+        if hook_point is not None:
+            self.value = hook_point(self.value)
+        return self.value
+
+    def __repr__(self):
+        init_kwargs_str = ", ".join(f"{k}={v!r}" for k, v in self._init_kwargs.items())
+        return f"{self.__class__.__name__}({init_kwargs_str})"
+
+
 class InputNode(DAGModule):
     """A node that takes an input value"""
-    def __init__(self):
-        super().__init__()
-        self.value = None
 
-    def forward(self, parent_values: dict[str, torch.Tensor]) -> torch.Tensor:
+    def compute_value(self, parent_values: dict[str, torch.Tensor]) -> torch.Tensor:
         """Return the value of the node
-        
+
         Parameters
         ----------
         parent_values : dict[str, torch.Tensor]
             The values of the parents of the node (ignored)
-        
+
         Returns
         -------
         value : torch.Tensor
             The value of the node
         """
         return self.value
-    
+
+
 class EqualityNode(DAGModule):
     """A node that checks if two inputs are equal"""
 
-    def forward(self, parent_values: dict[str, torch.Tensor]) -> torch.Tensor:
-        """Return the value of the node
-        
+    def compute_value(self, parent_values: dict[str, torch.Tensor]) -> torch.BoolTensor:
+        """Return a boolean tensor indicating where the two parent values are equal
+
         Parameters
         ----------
         parent_values : dict[str, torch.Tensor]
             The values of the parents of the node. There must be two parents.
-        
+
         Returns
         -------
-        value : torch.Tensor (boolean)
+        value : torch.BoolTensor
             A boolean tensor indicating where the two inputs are equal
         """
         if len(parent_values) != 2:
             raise ValueError("EqualityNode must have exactly two parents")
         values = list(parent_values.values())
         return values[0] == values[1]
+
+
+class CumSumNode(DAGModule):
+    """A node that computes the cumulative sum of its parents"""
+
+    def __init__(self, dim: int = -1):
+        super().__init__(dim=dim)
+        self.dim = dim
+
+    def compute_value(self, parent_values: dict[str, torch.Tensor]) -> torch.Tensor:
+        """Compute the cumulative sum of the input
+
+        Parameters
+        ----------
+        parent_values : dict[str, torch.Tensor]
+            The values of the parents of the node. There must be one parent.
+
+        Returns
+        -------
+        value : torch.Tensor
+            The cumulative sum of the input
+        """
+        if len(parent_values) != 1:
+            raise ValueError("CumSumNode must have exactly one parent")
+        values = list(parent_values.values())
+        return torch.cumsum(values[0], dim=self.dim)
