f2d495d1d9d0f80042918134d2075d8f51477268
Sam Adam-Day <me@samadamday.com>
2023-07-31 15:55:03 +0100

    Created fine-tune script

---
+++ experiments/scripts/components/fine_tune.py
@@ -0,0 +1,243 @@
+from copy import deepcopy
+from math import sqrt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+
+import einops
+
+import numpy as np
+
+import pandas as pd
+
+
+from tqdm import tqdm
+
+from transformer_lens import HookedTransformer
+
+
+def fine_tune_paren_bal(
+    base_model_name: str,
+    text_dataset_file: str,
+    device: torch.device | str = "cuda",
+    open_paren_str_tokens: list = ["("],
+    close_paren_str_tokens: list = [")"],
+    test_dataset_size: float = 0.1,
+    validation_dataset_size: float = 0.1,
+    batch_size: int = 256,
+    learning_rate: float = 1e-4,
+    num_epochs: int = 10,
+    lr_scheduler_patience: int = 1000,
+    seed: int = 2384,
+):
+    
+    #####################
+    # Set the random seed
+    #####################
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    #####################
+    # Load the model
+    #####################
+
+    print("Loading model...")
+
+    base_model = HookedTransformer.from_pretrained(base_model_name, device=device)
+
+    model_config = deepcopy(base_model.cfg)
+    model_config.d_vocab_out = 2
+
+    model = HookedTransformer(cfg=model_config)
+
+    model_state_dict = deepcopy(base_model.state_dict())
+    model_state_dict["unembed.W_U"] = torch.empty(
+        model_config.d_model, model_config.d_vocab_out
+    ).to(device)
+    model_state_dict["unembed.b_U"] = torch.empty(model_config.d_vocab_out).to(device)
+    nn.init.uniform_(
+        model_state_dict["unembed.W_U"],
+        -1 / sqrt(model_config.d_model),
+        1 / sqrt(model_config.d_model),
+    )
+    nn.init.uniform_(
+        model_state_dict["unembed.b_U"],
+        -1 / sqrt(model_config.d_model),
+        1 / sqrt(model_config.d_model),
+    )
+
+    model.load_state_dict(model_state_dict)
+
+    pad_token_id = base_model.tokenizer.pad_token_id
+
+    #####################
+    # Create the dataset
+    #####################
+
+    print("Creating dataset...")
+
+    open_paren_tokens = model.to_tokens(
+        open_paren_str_tokens, prepend_bos=False, move_to_device=False, truncate=False
+    )
+    open_paren_tokens = [
+        open_paren_tokens[i, 0].item() for i in range(open_paren_tokens.shape[0])
+    ]
+
+    closed_paren_tokens = model.to_tokens(
+        close_paren_str_tokens, prepend_bos=False, move_to_device=False, truncate=False
+    )
+    closed_paren_tokens = [
+        closed_paren_tokens[i, 0].item() for i in range(closed_paren_tokens.shape[0])
+    ]
+
+    text_data = pd.read_csv(text_dataset_file)
+    text_data_tokenised = base_model.to_tokens(
+        text_data["text"].values, move_to_device=False
+    )
+
+    open_bracket = torch.isin(text_data_tokenised, torch.tensor(open_paren_tokens))
+    closed_bracket = torch.isin(text_data_tokenised, torch.tensor(closed_paren_tokens))
+    bracket_values = torch.zeros_like(text_data_tokenised, dtype=torch.long)
+    bracket_values = bracket_values + open_bracket.long() - closed_bracket.long()
+    cumsum = torch.cumsum(bracket_values, dim=-1)
+    output_data = (cumsum > 0).to(dtype=torch.long)
+
+    loss_mask = text_data_tokenised != pad_token_id
+
+    shuffled_indices = torch.randperm(text_data_tokenised.shape[0])
+    train_indices = shuffled_indices[
+        : int(
+            text_data_tokenised.shape[0]
+            * (1 - test_dataset_size - validation_dataset_size)
+        )
+    ]
+    validation_indices = shuffled_indices[
+        int(
+            text_data_tokenised.shape[0]
+            * (1 - test_dataset_size - validation_dataset_size)
+        ) : int(text_data_tokenised.shape[0] * (1 - test_dataset_size))
+    ]
+    test_indices = shuffled_indices[
+        int(text_data_tokenised.shape[0] * (1 - test_dataset_size)) :
+    ]
+
+    train_dataset = TensorDataset(
+        text_data_tokenised[train_indices],
+        output_data[train_indices],
+        loss_mask[train_indices],
+    )
+    validation_dataset = TensorDataset(
+        text_data_tokenised[validation_indices],
+        output_data[validation_indices],
+        loss_mask[validation_indices],
+    )
+    test_dataset = TensorDataset(
+        text_data_tokenised[test_indices],
+        output_data[test_indices],
+        loss_mask[test_indices],
+    )
+
+    #####################
+    # Train the model
+    #####################
+
+    print("Training model...")
+
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=False, drop_last=True
+    )
+    validation_dataloader = DataLoader(
+        validation_dataset, batch_size=batch_size, shuffle=False, drop_last=True
+    )
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, drop_last=True
+    )
+
+    model.train()
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, "min", patience=lr_scheduler_patience, verbose=True
+    )
+    losses = np.empty(num_epochs)
+    accuracies = np.empty(num_epochs)
+
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+        total_agreement = 0.0
+
+        iterator = tqdm(
+            train_dataloader,
+            total=len(train_dataloader),
+            desc=f"Epoch {epoch + 1}/{num_epochs}",
+        )
+        for tokens, gold_output, loss_mask in iterator:
+            tokens = tokens.to(device)
+            gold_output = gold_output.to(device)
+            loss_mask = loss_mask.to(device)
+
+            optimizer.zero_grad()
+
+            output = model(tokens)
+            output_rearranged = einops.rearrange(
+                output, "batch seq out -> batch out seq"
+            )
+            loss = F.cross_entropy(output_rearranged, gold_output, reduction="none")
+            loss = loss[loss_mask].mean()
+            loss.backward()
+            optimizer.step()
+            scheduler.step(loss)
+
+            total_loss += loss.item()
+            with torch.no_grad():
+                total_agreement += (
+                    (torch.argmax(output, dim=-1) == gold_output).float().mean().item()
+                )
+
+        losses[epoch] = total_loss / len(train_dataloader)
+        accuracies[epoch] = total_agreement / len(train_dataloader)
+
+        print(f"Loss: {losses[epoch]:.4f}, Accuracy: {accuracies[epoch]:.4f}")
+
+    #####################
+    # Evaluate the model
+    #####################
+
+    print("Evaluating model...")
+
+    model.eval()
+
+    total_agreement = 0.0
+
+    iterator = tqdm(
+        validation_dataloader,
+        total=len(validation_dataloader),
+        desc=f"Eval",
+    )
+    with torch.no_grad():
+        for tokens, gold_output, loss_mask in iterator:
+            tokens = tokens.to(device)
+            gold_output = gold_output.to(device)
+            loss_mask = loss_mask.to(device)
+            output = model(tokens)
+            total_agreement += (
+                (torch.argmax(output, dim=-1) == gold_output).float().mean().item()
+            )
+    val_accuracy = total_agreement / len(validation_dataloader)
+
+    print(f"Eval accuracy: {val_accuracy:.4f}")
+
+    #####################
+    # Return the results
+    #####################
+
+    results = {
+        "train_losses": losses,
+        "train_accuracies": accuracies,
+        "val_accuracy": val_accuracy,
+    }
+
+    return results
---
+++ experiments/scripts/fine_tune_paren_bal.py
@@ -0,0 +1,145 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import os
+import json
+from datetime import datetime
+
+import torch
+
+from sklearn.model_selection import ParameterGrid
+
+from components import fine_tune_paren_bal
+
+BASE_MODEL_NAME = "gelu-1l"
+
+SCRIPT_PATH = os.path.realpath(__file__)
+PROJECT_DIR = os.path.normpath(SCRIPT_PATH + "/../../..")
+DATASET_DIR = os.path.join(PROJECT_DIR, "data")
+EXPERIMENT_RESULTS_DIR = os.path.join(PROJECT_DIR, "experiments", "results")
+TEXT_DATASET_NAME = "single_line.csv"
+TEXT_DATASET_FILE = os.path.join(DATASET_DIR, "paren-balancing", TEXT_DATASET_NAME)
+
+device = torch.device("cuda")
+
+
+# Set up the arg parser
+parser = ArgumentParser(
+    description="Run the parenthesis balancing experiments",
+    formatter_class=ArgumentDefaultsHelpFormatter,
+)
+
+# Add various arguments
+parser.add_argument(
+    "--combo-groups",
+    type=int,
+    default=1,
+    help="Into how many groups to split the experiment combinations",
+)
+parser.add_argument(
+    "--combo-num",
+    type=int,
+    default=0,
+    help="Which combo group to run this time",
+)
+parser.add_argument(
+    "--num-skip",
+    type=int,
+    default=0,
+    help="The number of initial combos to skip. Useful to resume a group",
+)
+parser.add_argument(
+    "--gpu-num", type=int, default=0, help="The (0-indexed) GPU number to use"
+)
+
+# Get the arguments
+cmd_args = parser.parse_args()
+
+
+# The different hyperparameters to test
+param_grid = {
+    "seed": [2384],
+    "batch_size": [256],
+    "learning_rate": [1e-1, 1e-2, 1e-3, 1e-4],
+    "num_epochs": [0],
+    "lr_scheduler_patience": [1000],
+}
+
+# An iterator over the configurations of hyperparameters
+param_iter = ParameterGrid(param_grid)
+
+# Enumerate these to keep track of them
+combinations = enumerate(param_iter)
+
+# Filter to combos
+combinations = filter(
+    lambda x: x[0] % cmd_args.combo_groups == cmd_args.combo_num, combinations
+)
+combinations = list(combinations)[cmd_args.num_skip :]
+
+# Keep track of the results of the runs
+run_results = []
+for combo_num in range(len(combinations)):
+    run_results.append("SKIPPED")
+
+try:
+    # Run the experiment for each sampled combination of parameters
+    for i, (combo_index, combo) in enumerate(combinations):
+        # Set the status of the current run to failed until proven otherwise
+        run_results[i] = "FAILED"
+
+        time_now = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
+
+        # Create a unique run_id for this trial
+        run_id = f"fine_tune_paren_bal_{combo_index}_{time_now}"
+
+        # Print the run_id and the Parameters
+        print()
+        print()
+        print("=" * 79)
+        title = f"| FINE-TUNING PAREN BAL | Run ID: {run_id}"
+        title += (" " * (78 - len(title))) + "|"
+        print(title)
+        print("=" * 79)
+        print()
+        print()
+
+        results = fine_tune_paren_bal(
+            base_model_name=BASE_MODEL_NAME,
+            text_dataset_file=TEXT_DATASET_FILE,
+            device=device,
+            batch_size=combo["batch_size"],
+            learning_rate=combo["learning_rate"],
+            num_epochs=combo["num_epochs"],
+            lr_scheduler_patience=combo["lr_scheduler_patience"],
+            seed=combo["seed"],
+        )
+
+        print("Saving results...")
+        results["train_losses"] = list(results["train_losses"])
+        results["train_accuracies"] = list(results["train_accuracies"])
+        results["base_model_name"] = BASE_MODEL_NAME
+        results["text_dataset_name"] = TEXT_DATASET_NAME
+        results["run_id"] = run_id
+        results["combo_index"] = combo_index
+        results["parameters"] = combo
+        with open(
+            os.path.join(EXPERIMENT_RESULTS_DIR, f"{run_id}.json"),
+            "w",
+        ) as f:
+            json.dump(results, f)
+
+        run_results[i] = "SUCCEEDED"
+
+finally:
+    # Print a summary of the experiment results
+    print()
+    print()
+    print("=" * 79)
+    title = f"| SUMMARY | GROUP {cmd_args.combo_num}/{cmd_args.combo_groups}"
+    title += (" " * (78 - len(title))) + "|"
+    print(title)
+    print("=" * 79)
+    for result, (combo_num, combo) in zip(run_results, combinations):
+        print()
+        print(f"COMBO {combo_num}")
+        print(combo)
+        print(result)
